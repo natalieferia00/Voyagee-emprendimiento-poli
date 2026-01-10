db7e5d8c9bea9a0ceaab54c0fe34479960f6fa81
natalie <natalieferia0@gmail.com>
2026-01-10 12:26:02 -0500

    commit

--- src/app/layout/component/app.topbar.ts
+++ src/app/layout/component/app.topbar.ts
@@ -33,15 +33,13 @@ import { ConversorMonedaComponent } from '../component/conversor-moneda/converso
             <button class="layout-menu-button layout-topbar-action" (click)="layoutService.onMenuToggle()">
                 <i class="pi pi-bars"></i>
             </button>
+
             <a class="layout-topbar-logo" routerLink="/">
-                <svg viewBox="0 0 54 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M17.1637 19.2467C17.1566 19.4033 17.1529 19.561 17.1529 19.7194C17.1529 25.3503 21.7203 29.915 27.3546 29.915C32.9887 29.915 37.5561 25.3503 37.5561 19.7194C37.5561 19.5572 37.5524 19.3959 37.5449 19.2355C38.5617 19.0801 39.5759 18.9013 40.5867 18.6994L40.6926 18.6782C40.7191 19.0218 40.7326 19.369 40.7326 19.7194C40.7326 27.1036 34.743 33.0896 27.3546 33.0896C19.966 33.0896 13.9765 27.1036 13.9765 19.7194C13.9765 19.374 13.9896 19.0316 14.0154 18.6927L14.0486 18.6994C15.0837 18.9062 16.1223 19.0886 17.1637 19.2467Z"
-                        fill="var(--primary-color)"
-                    />
+               
+                <svg width="14" height="14" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg" style="margin-right: 0.5rem;">
+                    <circle cx="7" cy="7" r="6" fill="var(--primary-color)" />
                 </svg>
+
                 <span>Voyagee</span>
             </a>
         </div>
@@ -53,7 +51,13 @@ import { ConversorMonedaComponent } from '../component/conversor-moneda/converso
                 </button>
 
                 <div class="relative">
-                    <button class="layout-topbar-action layout-topbar-action-highlight" pStyleClass="@next" enterFromClass="hidden" enterActiveClass="animate-scalein" leaveToClass="hidden" leaveActiveClass="animate-fadeout" [hideOnOutsideClick]="true">
+                    <button class="layout-topbar-action layout-topbar-action-highlight"
+                        pStyleClass="@next"
+                        enterFromClass="hidden"
+                        enterActiveClass="animate-scalein"
+                        leaveToClass="hidden"
+                        leaveActiveClass="animate-fadeout"
+                        [hideOnOutsideClick]="true">
                         <i class="pi pi-palette"></i>
                     </button>
                     <app-configurator />
@@ -62,7 +66,7 @@ import { ConversorMonedaComponent } from '../component/conversor-moneda/converso
 
             <div class="layout-topbar-menu hidden lg:block">
                 <div class="layout-topbar-menu-content flex flex-column gap-2">
-                    
+
                     <div class="relative">
                         <button type="button" class="layout-topbar-action w-full justify-content-start" (click)="toggleCalendar()">
                             <i class="pi pi-calendar mr-2"></i>
@@ -86,6 +90,7 @@ import { ConversorMonedaComponent } from '../component/conversor-moneda/converso
                             <span>Profile</span>
                         </button>
                     </div>
+
                 </div>
             </div>
         </div>
@@ -99,9 +104,9 @@ import { ConversorMonedaComponent } from '../component/conversor-moneda/converso
         [draggable]="false" 
         [resizable]="false"
         [breakpoints]="{ '960px': '75vw', '640px': '90vw' }">
-        
+
         <app-conversor-moneda></app-conversor-moneda>
-        
+
     </p-dialog>
 
     <div *ngIf="mostrarPerfilCard" class="fixed top-20 right-4 z-50 animate-fadein">
@@ -109,11 +114,20 @@ import { ConversorMonedaComponent } from '../component/conversor-moneda/converso
     </div>
     `,
     styles: [`
-        .animate-fadein { animation: fadeIn 0.3s ease-in-out; }
-        @keyframes fadeIn { from { opacity: 0; transform: translateY(-10px); } to { opacity: 1; transform: translateY(0); } }
-        :host ::ng-deep .p-datepicker-inline { border: none; background: var(--surface-card); }
-        
-        /* Ajuste para que los botones verticales se vean bien */
+        .animate-fadein {
+            animation: fadeIn 0.3s ease-in-out;
+        }
+
+        @keyframes fadeIn {
+            from { opacity: 0; transform: translateY(-10px); }
+            to { opacity: 1; transform: translateY(0); }
+        }
+
+        :host ::ng-deep .p-datepicker-inline {
+            border: none;
+            background: var(--surface-card);
+        }
+
         .layout-topbar-action.w-full {
             display: flex;
             align-items: center;
@@ -125,14 +139,17 @@ import { ConversorMonedaComponent } from '../component/conversor-moneda/converso
 export class AppTopbar {
     mostrarPerfilCard = false;
     mostrarCalendario = false;
-    mostrarConversor = false; 
-    
+    mostrarConversor = false;
+
     fechaSeleccionada: Date = new Date();
 
     constructor(public layoutService: LayoutService) {}
 
     toggleDarkMode() {
-        this.layoutService.layoutConfig.update((state) => ({ ...state, darkTheme: !state.darkTheme }));
+        this.layoutService.layoutConfig.update(state => ({
+            ...state,
+            darkTheme: !state.darkTheme
+        }));
     }
 
     togglePerfilCard() {
